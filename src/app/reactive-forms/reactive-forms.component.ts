26792165800a4679ec800225dce01144660f8cfd
Velupriya M <velupriya@thinkbridge.in>
2020-06-22 08:58:25 +0530

    validations

--- src/app/reactive-forms/reactive-forms.component.ts
+++ src/app/reactive-forms/reactive-forms.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { IfStmt } from '@angular/compiler';
+import { RegexPattern } from 'src/app/shared/regexPattern';
 
 @Component({
   selector: 'app-reactive-forms',
@@ -9,20 +11,43 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class ReactiveFormsComponent implements OnInit {
 
   loginForm: FormGroup;
+  userNameValidators: any = [Validators.required, Validators.maxLength(10), Validators.minLength(5)];
+  submitted = false;
   constructor() { }
   ngOnInit(): void {
     this.loginForm = new FormGroup({
-      userName: new FormControl('', Validators.required),
-      password: new FormControl()
+      userName: new FormControl('', [Validators.required, this.customValidator]),
+      password: new FormControl('',
+        [Validators.pattern(RegexPattern.PASSWORD_STRENGTH),
+        Validators.required, Validators.max(10)])
     });
   }
 
   onSubmit(): void {
-    console.log(this.loginForm.value);
-    alert(JSON.stringify(this.loginForm.value));
-    if(true) {
-      console.log('');
+    this.submitted = true;
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+      alert(JSON.stringify(this.loginForm.value));
     }
+  }
+
+  customValidator(control: any) {
+    const val = control?.value;
+  //  const isValidForm = this.loginForm?.valid;
+    if (val !== '') {
+      if (val.length < 5) {
+        return {
+          customlength: true
+        };
+      } else {
+        return null;
+      }
+    }
+    return null;
+  }
+
+  get f() {
+    return this.loginForm.controls;
   }
 
 }
