316cc02393b678de521d6ad772314bb0773cc716
Velupriya M <velupriya@thinkbridge.in>
2020-06-25 08:50:18 +0530

    routing

---
+++ src/app/Exams/ug/ug.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ug',
+  templateUrl: './ug.component.html',
+  styleUrls: ['./ug.component.scss']
+})
+export class UGComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,41 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.component';
+import { FirstPageManualComponent } from './first-page-manual/first-page-manual.component';
+import { MyFirstPageComponent } from './my-first-page/my-first-page.component';
+import { TemplateDrivenComponent } from './template-driven/template-driven.component';
+import { ExamsModule } from './Exams/exams.module';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'reactive-forms',
+    component: ReactiveFormsComponent
+  },
+  {
+    path: 'first-page-manual',
+    component: FirstPageManualComponent
+  },
+  {
+    path: 'my-first-page',
+    component: MyFirstPageComponent
+  },
+  {
+    path: 'template-driven',
+    component: TemplateDrivenComponent
+  },
+  // {
+  //   path: '**',
+  //   redirectTo: 'first-page-manual'
+  // },
+  {
+    path: 'exams',
+    loadChildren: () => ExamsModule
+    // src\app\Exams\exams.module.ts
+  }
+  // loadChildren: () => TestModule
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
